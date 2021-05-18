52408a9d693db7b6f69778971dca0af4b1bc3c5b
DUNG TA DINH <tddung@rd.local>
2021-05-18 17:36:12 +0700

    clone-form ver 1

---
+++ MISA.AMIS.UI/src/components/dialogs/Test.vue
@@ -0,0 +1,302 @@
+<template>
+  <div>
+    <div class="detail-dialog-content" style="background-color: #68718d">
+      <div class="detail-dialog-header">
+        <div class="detail-header-title">
+          <div class="wrap-header-title">
+            <div class="header-title-text">Thông tin nhân viên</div>
+            <div class="header-title-customer">
+              <input type="checkbox" class="customer-checkbox" />
+              <div class="customer-content">Là khách hàng</div>
+            </div>
+            <div class="header-title-customer">
+              <input type="checkbox" class="customer-checkbox" />
+              <div class="customer-content">Là nhà cung cấp</div>
+            </div>
+          </div>
+        </div>
+        <div class="detail-header-close">
+          <div class="icon icon-24 detail-icon-help"></div>
+          <div class="icon icon-24 detail-icon-close"></div>
+        </div>
+      </div>
+      <div class="detail-dialog-main">
+        <div class="layer-detail-inf">
+          <div style="display: flex !important">
+            <div style="padding-right: 26px; width: 50% !important">
+              <div class="display: flex !important">
+                <div style="padding-right: 6px; width: 40% !important">
+                  <div style="display: flex !important">
+                    <div class="input-title">Mã</div>
+                    <div class="input-title-require">&nbsp;*</div>
+                  </div>
+                  <div class="detail-inf-label">
+                    <input type="text" class="detail-inf-input" />
+                  </div>
+                </div>
+                <div style="width: 60% !important">
+                  <div style="position: relative; width: 100%">
+                    <div style="display: flex !important">
+                      <div class="input-title">Tên</div>
+                      <div class="input-title-require">&nbsp;*</div>
+                    </div>
+                    <div class="detail-inf-label">
+                      <input type="text" class="detail-inf-input" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="detail-inf-lable">
+                <div style="width: 100% !important">
+                  <div style="display: flex !important">
+                    <div class="input-title">Đơn vị</div>
+                    <div class="input-title-require">&nbsp;*</div>
+                  </div>
+                  <div class="detail-inf-label">
+                    <input type="text" class="detail-inf-input" />
+                  </div>
+                </div>
+              </div>
+              <div class="detail-inf-lable">
+                <div style="width: 100% !important">
+                  <div style="width: 100% !important">
+                    <div style="display: flex !important">
+                      <div class="input-title">Chức danh</div>
+                    </div>
+                    <div class="detail-inf-label">
+                      <input type="text" class="detail-inf-input" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div style="width: 50% !important">
+              <div style="display: flex !important">
+                <div style="padding-right: 6px; width: 40% !important">
+                  <div class="detail-inf-lable">
+                    <div style="display: flex !important">
+                      <div class="input-title">Chức danh</div>
+                    </div>
+                    <div class="detail-inf-label">
+                      <input type="text" class="detail-inf-input" />
+                    </div>
+                  </div>
+                </div>
+                <div style="padding-right: 6px; width: 60% !important">
+                  <div class="detail-inf-lable">
+                    <div style="display: flex !important">
+                      <div class="input-title">Chức danh</div>
+                    </div>
+                    <div class="detail-inf-label">
+                      <input type="text" class="detail-inf-input" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="display: flex !important">
+                <div style="padding-right: 6px; width: 40% !important">
+                  <div class="detail-inf-lable">
+                    <div style="display: flex !important">
+                      <div class="input-title">Chức danh</div>
+                    </div>
+                    <div class="detail-inf-label">
+                      <input type="text" class="detail-inf-input" />
+                    </div>
+                  </div>
+                </div>
+                <div style="padding-right: 6px; width: 60% !important">
+                  <div class="detail-inf-lable">
+                    <div style="display: flex !important">
+                      <div class="input-title">Chức danh</div>
+                    </div>
+                    <div class="detail-inf-label">
+                      <input type="text" class="detail-inf-input" />
+                    </div>
+                  </div>
+                </div>
+                <div
+                  style="padding-bottom: 12px; display: flex !important"
+                ></div>
+              </div>
+              <div class="detail-inf-lable">
+                <div style="width: 100% !important">
+                  <div style="width: 100% !important">
+                    <div style="display: flex !important">
+                      <div class="input-title">Chức danh</div>
+                    </div>
+                    <div class="detail-inf-label">
+                      <input type="text" class="detail-inf-input" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="layer-detail-inf">
+          <div class="layer-detail-inf">
+            <div style="display: flex !important">
+              <div class="input-title">Đơn vị</div>
+            </div>
+            <div class="detail-inf-label">
+              <input type="text" class="detail-inf-input" />
+            </div>
+            <div class="detail-inf-title">
+              <div style="padding-right: 6px; width: 25% !important">
+                <div class="detail-inf-lable">
+                  <div style="display: flex !important">
+                    <div class="input-title">Chức danh</div>
+                  </div>
+                  <div class="detail-inf-label">
+                    <input type="text" class="detail-inf-input" />
+                  </div>
+                </div>
+              </div>
+              <div style="padding-right: 6px; width: 25% !important">
+                <div class="detail-inf-lable">
+                  <div style="display: flex !important">
+                    <div class="input-title">Chức danh</div>
+                  </div>
+                  <div class="detail-inf-label">
+                    <input type="text" class="detail-inf-input" />
+                  </div>
+                </div>
+              </div>
+              <div style="padding-right: 6px; width: 25% !important">
+                <div class="detail-inf-lable">
+                  <div style="display: flex !important">
+                    <div class="input-title">Chức danh</div>
+                  </div>
+                  <div class="detail-inf-label">
+                    <input type="text" class="detail-inf-input" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="detail-dialog-footer"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.detail-dialog-content {
+  position: relative;
+  transition: all 0.2s;
+  z-index: 200;
+  max-width: calc(100% - 30px);
+  box-shadow: 0 5px 20px 0 rgb(0 0 0 / 10%);
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  min-width: 900px;
+  max-width: 900px;
+  width: 900px;
+}
+.detail-dialog-header {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  border-radius: 5px 5px;
+  position: relative;
+}
+.detail-header-title {
+  width: 100%;
+  padding: 20px 32px 20px 32px;
+  padding-right: 12px;
+  position: relative;
+  overflow: hidden;
+  border-radius: 5px 5px 0 0;
+}
+.wrap-header-title {
+  display: flex;
+}
+.header-title-text {
+  font-size: 24px;
+  color: #111;
+  font-weight: 700;
+}
+.header-title-customer {
+  padding-left: 1.5rem !important;
+  padding-right: 1.5rem !important;
+  justify-content: flex-start;
+  position: relative;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+.customer-checkbox {
+  position: absolute;
+  width: 10%;
+  height: 100%;
+  z-index: 200;
+  cursor: pointer;
+  z-index: 10;
+}
+.customer-content {
+  white-space: nowrap;
+  padding-left: 10px;
+  font-size: 13px;
+}
+.detail-header-close {
+  display: flex;
+  padding: 12px;
+}
+.detail-icon-help {
+  background-position: -89px -144px !important;
+}
+.detail-icon-close {
+  background-position: -144px -144px !important;
+}
+.detail-dialog-main {
+  transition: all 0.23s ease 0.1s;
+  flex-direction: column;
+  display: flex;
+  overflow: auto;
+  overflow-y: visible;
+  padding: 0 32px 20px 32px;
+}
+.layer-detail-inf {
+  padding-bottom: 12px;
+}
+.detail-inf-title {
+  padding-bottom: 12px;
+  display: flex !important;
+}
+.input-title {
+  font-size: 12px;
+  font-weight: 700;
+  padding-bottom: 4px;
+  white-space: nowrap;
+}
+.input-title-require {
+  color: red;
+}
+.detail-inf-label {
+  position: relative;
+  width: 100%;
+  padding-bottom: 12px;
+}
+
+.detail-inf-input {
+  font-size: 13px;
+  height: 32px;
+  color: inherit;
+  position: relative;
+  padding: 6px 10px;
+  border-radius: 2px;
+  border: 1px solid #babec5;
+  box-sizing: border-box;
+  width: 100%;
+  background-color: #fff;
+}
+.w-25 {
+  width: 25%;
+}
+</style>
--- MISA.AMIS.UI/src/components/layouts/TheContent.vue
+++ MISA.AMIS.UI/src/components/layouts/TheContent.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="content">
-    <EmployeeList />
+    <!-- <EmployeeList /> -->
+    <Test />
   </div>
 </template>
 
 <script>
-import EmployeeList from "../pages/EmployeeList";
+// import EmployeeList from "../pages/EmployeeList";
+import Test from "../dialogs/Test";
 export default {
   name: "TheContent",
   data() {
     return {};
   },
   components: {
-    EmployeeList,
+    Test,
+    // EmployeeList,
   },
   methods: {},
 };
@@ -22,5 +25,6 @@ export default {
   height: calc(100% - 48px);
   padding: 0 30px 0 20px;
   background-color: #f4f5f8;
+  /* background-color: #68718d; */
 }
 </style>
