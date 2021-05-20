f8c8488390a076cd797cc514544b0579e753b213
dungxtd <dungxtd20@gmail.com>
2021-05-21 03:28:02 +0700

    hicccc

--- MISA.AMIS.API/MISA.Core/Interface/Repository/IEmployeeRepository.cs
+++ MISA.AMIS.API/MISA.Core/Interface/Repository/IEmployeeRepository.cs
@@ -9,10 +9,43 @@ namespace MISA.Core.Interface.Repository
 {
     public interface IEmployeeRepository : IBaseRepository<Employee>
     {
+        /// <summary>
+        /// Check mã nhân viên tồn tại
+        /// </summary>
+        /// <param name="employeeCode">Mã nhân viên</param>
+        /// <returns>true: đã tồn tại</returns>
+        /// <returns>false: chưa tồn tại</returns>
         public IEnumerable<bool> CheckEmployeeCodeExits(string employeeCode);
+
+        /// <summary>
+        /// Lấy page theo filter
+        /// </summary>
+        /// <param name="pageIndex">số thứ tự trang</param>
+        /// <param name="pageSize">số bản ghi trên 1 trang</param>
+        /// <param name="filter">bộ lọc</param>
+        /// <returns>mảng nhân viên và tổng số bản ghi</returns>
         Object GetPaging(int pageIndex, int pageSize,string filter);
+
+        /// <summary>
+        /// Lấy mã lớn nhất
+        /// </summary>
+        /// <returns>Mã lớn nhất trong hệ thống</returns>
         public String GetMaxCode();
+
+        /// <summary>
+        /// Check code trùng khi update
+        /// </summary>
+        /// <param name="employeeCode">Mã nhân viên</param>
+        /// <param name="employeeId">Id nhân viên</param>
+        /// <returns>true: trùng</returns>
+        /// <returns>false: không trùng</returns>
         public IEnumerable<bool> CheckEmployeeCodeExitsUpdate(string employeeCode, Guid employeeId);
+
+        /// <summary>
+        /// Clone bản ghi và thêm mã lớn nhất
+        /// </summary>
+        /// <param name="entityId">Id nhân viên</param>
+        /// <returns>bản ghi đã chọn</returns>
         public Employee GetEmployeeMaxCodeById(Guid entityId);
     }
 }
--- MISA.AMIS.API/MISA.Core/Interface/Service/IEmployeeService.cs
+++ MISA.AMIS.API/MISA.Core/Interface/Service/IEmployeeService.cs
@@ -13,9 +13,32 @@ namespace MISA.Core.Interface.Service
     /// CreatedBy: TDDUNG (27/4/2021)
     public interface IEmployeeService : IBaseService<Employee>
     {
+        /// <summary>
+        /// Service lấy trang theo bộ lọc
+        /// </summary>
+        /// <param name="pageIndex">số thứ tự trang</param>
+        /// <param name="pageSize">số bản ghi trên 1 trang</param>
+        /// <param name="filter">bộ lọc</param>
+        /// <returns>mảng nhân viên và tổng số bản ghi</returns>
         Object GetPaging(int pageIndex, int pageSize, string filter);
+
+        /// <summary>
+        /// Service mã lớn nhất
+        /// </summary>
+        /// <returns>Mã lớn nhất trong hệ thống</returns>
         String GetMaxCode();
+
+        /// <summary>
+        /// Service lấy bản ghi theo id nhưng có mã lớn nhất
+        /// </summary>
+        /// <param name="entityId"></param>
+        /// <returns>bản ghi đã chọn</returns>
         Employee GetEmployeeMaxCodeById(Guid entityId);
+
+        /// <summary>
+        /// Service xuất file excel
+        /// </summary>
+        /// <returns>file link</returns>
         FileExport Export();
     }
 }
